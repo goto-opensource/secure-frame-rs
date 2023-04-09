a39d2c8a1677ea7035d886814e82119d32a69031
Tobias Waurick <twaurick@proton.me>
2023-04-09 23:32:50 +0200

    test: convert cipher suite id to name

--- src/test_vectors/mod.rs
+++ src/test_vectors/mod.rs
@@ -3,11 +3,12 @@
 #![allow(clippy::missing_panics_doc)]
 
 extern crate serde;
+use phf::phf_map;
 
 pub fn get_test_vector(cipher_suite_variant: &str) -> &'static TestVector {
     TEST_VECTORS
         .iter()
-        .find(|v| dbg!(&v.cipher_suite_variant) == dbg!(cipher_suite_variant))
+        .find(|v| v.cipher_suite_variant == cipher_suite_variant)
         .unwrap()
 }
 
@@ -17,6 +18,13 @@ static ref TEST_VECTORS: Vec<TestVector> = {
      parse_test_vectors()
 };
 }
+
+const CIPHER_SUITE_NAME_FROM_ID: phf::Map<u8, &str> = phf_map! {
+        1u8 => "AesCm128HmacSha256_8",
+        2u8 => "AesCm128HmacSha256_4",
+        3u8 => "AesGcm128Sha256",
+        4u8 => "AesGcm256Sha512",
+};
 
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct EncryptionTestCase {
@@ -34,7 +42,10 @@ pub struct EncryptionTestCase {
 
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct TestVector {
-    #[serde(rename = "cipher_suite")]
+    #[serde(
+        rename = "cipher_suite",
+        deserialize_with = "cipher_suite_name_from_id"
+    )]
     pub cipher_suite_variant: String,
 
     #[serde(rename = "base_key", deserialize_with = "vec_from_hex_str")]
@@ -60,18 +71,34 @@ where
     hex::decode(hex_str).map_err(serde::de::Error::custom)
 }
 
+fn cipher_suite_name_from_id<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let id: u8 = serde::Deserialize::deserialize(deserializer)?;
+
+    if let Some(&name) = CIPHER_SUITE_NAME_FROM_ID.get(&id) {
+        Ok(String::from(name))
+    } else {
+        Err(serde::de::Error::custom(format!(
+            "Invalid cipher suite id {}",
+            id
+        )))
+    }
+}
+
 fn parse_test_vectors() -> Vec<TestVector> {
     serde_json::from_str(TEST_VECTORS_STR).unwrap()
 }
 
 #[cfg(test)]
 mod test {
-    use super::get_test_vector;
+    use super::{get_test_vector, CIPHER_SUITE_NAME_FROM_ID};
 
     #[test]
     fn should_parse_test_vectors() {
-        let valid_cipher_suite_variants = 1..4;
-        for variant in valid_cipher_suite_variants {
+        let valid_cipher_suite_variants = CIPHER_SUITE_NAME_FROM_ID.values();
+        for &variant in valid_cipher_suite_variants {
             let vector = get_test_vector(variant);
             assert_eq!(vector.cipher_suite_variant, variant);
         }
