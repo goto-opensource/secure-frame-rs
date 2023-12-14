15225052cdc64a34a85f3e1e68c0f521470575b2
Tobias Waurick <tobias.waurick@goto.com>
2023-12-14 12:43:06 +0100

    test: bench with random headers
    This way we have more variation, and the tests for serializationa and deseralization are more alike.
    also fixed a broken test.

--- benches/header_parsing.rs
+++ benches/header_parsing.rs
@@ -3,26 +3,45 @@
 
 use criterion::{black_box, criterion_group, Criterion};
 
+use rand::{thread_rng, Rng};
 use sframe::header::SframeHeader;
 
+fn create_random_values(size: usize) -> Vec<u64> {
+    let mut values = vec![0; size];
+    thread_rng().fill(values.as_mut_slice());
+    values
+}
+
+fn create_random_headers(size: usize) -> impl Iterator<Item = SframeHeader> {
+    let random_key_ids = create_random_values(size);
+    let random_frame_counts = create_random_values(size);
+
+    random_key_ids
+        .into_iter()
+        .zip(random_frame_counts)
+        .map(|(key_id, frame_count)| SframeHeader::new(key_id, frame_count))
+}
+
 fn header_serialization(c: &mut Criterion) {
-    c.bench_function("serialize header with short key", |b| {
-        let mut buffer = vec![0_u8; 4];
-        let basic_header = SframeHeader::new(7, 0);
-        b.iter(|| black_box(basic_header.serialize(&mut buffer)))
+    c.bench_function("serialize 1000 random headers", |b| {
+        let headers = create_random_headers(1000);
+        let mut headers_buffers = headers
+            .map(|header| {
+                let buffer = vec![0_u8; header.len()];
+                (header, buffer)
+            })
+            .collect::<Vec<_>>();
+        b.iter(move || {
+            headers_buffers
+                .iter_mut()
+                .for_each(|(header, ref mut buffer)| header.serialize(buffer).unwrap())
+        })
     });
 
-    c.bench_function("serialize extended header", |b| {
-        let mut buffer = vec![0_u8; 4];
-        let extended_header = SframeHeader::new(128, 0);
-        b.iter(|| black_box(extended_header.serialize(&mut buffer)))
-    });
-
-    c.bench_function("deserialize 1000 headers", |b| {
-        let serialized_headers = (0..1000_u64)
-            .map(|i| {
-                let header = SframeHeader::new(i, i);
-                let mut buffer = vec![0_u8; 4];
+    c.bench_function("deserialize 1000 random headers", |b| {
+        let serialized_headers = create_random_headers(1000)
+            .map(|header| {
+                let mut buffer = vec![0_u8; header.len()];
                 header.serialize(&mut buffer).unwrap();
                 buffer
             })
